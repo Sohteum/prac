e41572c95f8fea5407854d3fd226085ad68938cd
Sohteum <thtwk1123@gmail.com>
2023-10-05 14:38:38 +0900

    .

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +32,35 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호를 암호화시킨다
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (PlainPassword, cb) {
+  //plainPassword: 1234567 암호화된 비밀번호 어쩌구저쩌구랑 맞는지 체크를 함
+  bcrypt.compare(PlainPassword, this.password, function (err, inMatch) {
+    if (err) return cb(err),
+     cb(null, inMatch);
+  });
+};
+
+userSchema.methods.generateToken = function(cd){
+    // jsonwebtoken이용해서 토큰을 생성하기
+}
 
 const User = mongoose.model("User", userSchema);
 
