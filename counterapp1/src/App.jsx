a29418cfb6911fbd29f6507a0dfa8a62cd49812c
Sohteum <thtwk1123@gmail.com>
2024-03-25 22:20:05 +0900

    .

--- counterapp1/src/App.jsx
+++ counterapp1/src/App.jsx
@@ -1,11 +1,16 @@
 import Viewer from "./components/Viewer.jsx";
 import Controller from "./components/Controller.jsx";
 import "./App.css"
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
 
     const [count, setCount] = useState(0);
+    const [input, setInput] = useState("");
+
+    useEffect(()=>{
+      console.log(`count : ${count}, input: ${input}`)
+    }, [count, input])
 
     const onClickButton =(value)=>{
         setCount(count +value);
@@ -14,6 +19,11 @@ function App() {
     return (
         <div className='App'>
             <h1>Simple Counter</h1>
+          <section>
+            <input onChange={(e)=>{
+            setInput(e.target.value);}
+            }/>
+          </section>
             <section>
                 <Viewer count={count}/>
             </section>
