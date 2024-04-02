ee92f6ca82485c045e254b0ca420126c94d5eaae
Sohteum <thtwk1123@gmail.com>
2024-04-02 11:35:36 +0900

    .

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,13 +1,63 @@
-import { useState } from 'react'
+import {useState, useRef} from 'react'
 import './App.css'
+import Header from "./components/Header"
+import List from "./components/List"
+import Editor from "./components/Editor"
+import todoItem from "./components/TodoItem.jsx";
+
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래하기',
+    date: new Date().getTime(),
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => { //새로운 Todo아이템을 객체 형태로 만들어줌
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  }
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId
+          ? {...todo, isDone: !todo.isDone}
+          : todo
+      ))
+  }
 
   return (
-    <>
-<div>hello</div>
-    </>
+    <div className="App">
+      <Header/>
+      <Editor onCreate={onCreate}/>
+      <List todos={todos} onUpdate={onUpdate}/>
+    </div>
   )
 }
 
